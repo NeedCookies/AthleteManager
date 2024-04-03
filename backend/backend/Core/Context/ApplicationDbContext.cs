2c291117e7814afbdd33a16b8fd60f8d3d53affc
NeedCookies <emilkalim895@gmail.com>
2024-04-03 21:29:47 +0300

    Add Initial migrations

---
+++ backend/backend/Core/Context/ApplicationDbContext.cs
@@ -0,0 +1,45 @@
+﻿
+using backend.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Core.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        { 
+
+        }
+
+        public DbSet<Sport> Sports { get; set; }
+        public DbSet<Athlete> Athletes { get; set; }
+        public DbSet<Competition> Competitions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Competition>()
+                .HasOne(c => c.Sport)
+                .WithMany(s => s.Competitions)
+                .HasForeignKey(c => c.SportId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Athlete>()
+                .HasOne(a => a.Sport)
+                .WithMany(s => s.Athletes)
+                .HasForeignKey(a => a.SportId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Competition>()
+                .HasMany(c => c.competitors)
+                .WithMany(a => a.competitions);
+
+            modelBuilder.Entity<Competition>()
+                .HasOne(c => c.Winner)
+                .WithOne()
+                .HasForeignKey<Competition>(c => c.WinnerId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
---
+++ backend/backend/Migrations/20240403174327_Initial-Migration.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Sports",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sports", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Athletes",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LastName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Region = table.Column<int>(type: "int", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    SportId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Athletes", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Athletes_Sports_SportId",
+                        column: x => x.SportId,
+                        principalTable: "Sports",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Competitions",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    WinnerId = table.Column<int>(type: "int", nullable: false),
+                    Rang = table.Column<int>(type: "int", nullable: false),
+                    SportId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Competitions", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Competitions_Athletes_WinnerId",
+                        column: x => x.WinnerId,
+                        principalTable: "Athletes",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Competitions_Sports_SportId",
+                        column: x => x.SportId,
+                        principalTable: "Sports",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AthleteCompetition",
+                columns: table => new
+                {
+                    competitionsID = table.Column<int>(type: "int", nullable: false),
+                    competitorsID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AthleteCompetition", x => new { x.competitionsID, x.competitorsID });
+                    table.ForeignKey(
+                        name: "FK_AthleteCompetition_Athletes_competitorsID",
+                        column: x => x.competitorsID,
+                        principalTable: "Athletes",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AthleteCompetition_Competitions_competitionsID",
+                        column: x => x.competitionsID,
+                        principalTable: "Competitions",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AthleteCompetition_competitorsID",
+                table: "AthleteCompetition",
+                column: "competitorsID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Athletes_SportId",
+                table: "Athletes",
+                column: "SportId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Competitions_SportId",
+                table: "Competitions",
+                column: "SportId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Competitions_WinnerId",
+                table: "Competitions",
+                column: "WinnerId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AthleteCompetition");
+
+            migrationBuilder.DropTable(
+                name: "Competitions");
+
+            migrationBuilder.DropTable(
+                name: "Athletes");
+
+            migrationBuilder.DropTable(
+                name: "Sports");
+        }
+    }
+}
