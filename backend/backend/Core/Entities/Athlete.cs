3b0d7f9efd797af1d23a61ba02c99a5c570da7c2
NeedCookies <emilkalim895@gmail.com>
2024-04-03 20:46:33 +0300

    Change entities: athlete has list of competitions he took part; DbContext class add instroctions for escape cascade delete

--- backend/backend/Core/Entities/Athlete.cs
+++ backend/backend/Core/Entities/Athlete.cs
@@ -9,15 +9,15 @@ namespace backend.Core.Entities
         [MinLength(3)]
         [RegularExpression(@"^[A-ZА-ЯЁ][a-zа-яё\-]*$", ErrorMessage = "Имя должно начинаться с заглавной буквы и содержать только латинские буквы или кириллицу")]
         public string LastName { get; set; }
-        
         public DateTime BirthDate { get; set; }
         public Regions Region { get; set; }
+        [MaxLength(150)]
+        [MinLength(3)]
         [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Некорректный Email")]
         public string Email { get; set; }
 
+        public ICollection<Competition> competitions { get; set; }
         public int SportId { get; set; }
         public Sport Sport { get; set; }
-
-        public List<Competition> Competitions { get; set; }
     }
 }
--- backend/backend/Core/Entities/Competition.cs
+++ backend/backend/Core/Entities/Competition.cs
@@ -5,13 +5,13 @@ namespace backend.Core.Entities
     public class Competition : BaseEntity
     {
         public DateTime Date { get; set; }
-        public List<Athlete> Winners { get; set; }
-        public List<int> Prizes { get; set; }
+        public int WinnerId { get; set; }
+        public Athlete Winner { get; set; }
         public Rangs Rang { get; set; }
 
         public int SportId { get; set; }
         public Sport Sport { get; set; }
 
-        public ICollection<Athlete> Athletes { get; set; } 
+        public ICollection<Athlete> competitors { get; set; } 
     }
 }
