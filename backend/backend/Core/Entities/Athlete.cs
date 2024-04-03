1bab3fbef35f405fcf2a2e018de5328ab1e3e04f
NeedCookies <emilkalim895@gmail.com>
2024-04-03 20:00:33 +0300

    Change entities, competition has one winner<Athlete>, athltete doesn't have competitions list yet

--- backend/backend/Core/Entities/Athlete.cs
+++ backend/backend/Core/Entities/Athlete.cs
@@ -1,15 +1,22 @@
 ﻿using backend.Core.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace backend.Core.Entities
 {
     public class Athlete : BaseEntity
     {
+        [MaxLength(100)]
+        [MinLength(3)]
+        [RegularExpression(@"^[A-ZА-ЯЁ][a-zа-яё\-]*$", ErrorMessage = "Имя должно начинаться с заглавной буквы и содержать только латинские буквы или кириллицу")]
         public string LastName { get; set; }
+        
         public DateTime BirthDate { get; set; }
         public Regions Region { get; set; }
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Некорректный Email")]
         public string Email { get; set; }
 
         public int SportId { get; set; }
+        public Sport Sport { get; set; }
 
         public List<Competition> Competitions { get; set; }
     }
--- backend/backend/Core/Entities/BaseEntity.cs
+++ backend/backend/Core/Entities/BaseEntity.cs
@@ -6,6 +6,9 @@ namespace backend.Core.Entities
     {
         [Key]
         public int ID { get; set; }
+        [MaxLength(100)]
+        [MinLength(3)]
+        [RegularExpression(@"^[A-ZА-ЯЁ][a-zа-яё\-]*$", ErrorMessage = "Имя должно начинаться с заглавной буквы и содержать только латинские буквы или кириллицу")]
         public string Name { get; set; }
     }
 }
