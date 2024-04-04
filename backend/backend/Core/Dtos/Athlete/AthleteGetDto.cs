707408b2e692c6a39034e225333d720500ca0959
NeedCookies <emilkalim895@gmail.com>
2024-04-04 14:29:40 +0300

    Add controller(create, get), Dto model, automapper for Athlete entity

---
+++ backend/backend/Core/Dtos/Athlete/AthleteGetDto.cs
@@ -0,0 +1,17 @@
+﻿using backend.Core.Enums;
+using backend.Core.Entities;
+
+namespace backend.Core.Dtos.Athlete
+{
+    public class AthleteGetDto
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Regions Region { get; set; }
+        public string Email { get; set; }
+        public int SportId { get; set; }
+        public string SportName { get; set; }
+    }
+}
