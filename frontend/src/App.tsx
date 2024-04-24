1b63bb771fa650c9c94e2ca69ec5c263999d2275
NeedCookies <emilkalim895@gmail.com>
2024-04-24 23:11:10 +0300

    Finish Navbar functional (open/hide functions)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { useContext } from 'react';
 import { ThemeContext } from "./context/theme.context"
 import Navbar from './components/navbar/Navbar.component';
 
-function App() {
+const App = () => {
   const {darkMode} = useContext(ThemeContext);
 
   const appStyles = darkMode ? "app dark" : "app"
--- frontend/src/components/navbar/Navbar.component.tsx
+++ frontend/src/components/navbar/Navbar.component.tsx
@@ -1,42 +1,54 @@
-import './navbar.scss';
-import {Link} from "react-router-dom";
-import {Menu, LightMode, DarkMode} from "@mui/icons-material";
-import { ToggleButton } from '@mui/material';
-import { useContext } from 'react';
-import {ThemeContext} from "../../context/theme.context"
+import "./navbar.scss";
+import { Link } from "react-router-dom";
+import { Menu, LightMode, DarkMode } from "@mui/icons-material";
+import { ToggleButton } from "@mui/material";
+import { useContext, useState } from "react";
+import { ThemeContext } from "../../context/theme.context";
 
 const links = [
-    {href : "/", label: "Home"},
-    {href: "/sports", label: "Sports" },
-    {href: "/competitions", label: "Competitions"},
-    {href: "/athletes", label: "Athletes"}
-]
+  { href: "/", label: "Home" },
+  { href: "/sports", label: "Sports" },
+  { href: "/competitions", label: "Competitions" },
+  { href: "/athletes", label: "Athletes" },
+];
 
 const Navbar = () => {
-    const {darkMode, toggleDarkMode} = useContext(ThemeContext);
-    return (
-        <div className='navbar'>
-            <div className='brand'>
-                <span>Resume Management</span>
-            </div>
-            <div className='menu'></div>
-                <ul>
-                    {links.map((item) => (
-                        <li key={item.href}>
-                            <Link to={item.href}>{item.label}</Link>
-                        </li>
-                    ))}
-                </ul>
-            <div className='hmm'>
-                <Menu />
-            </div>
-            <div className='toggle'>
-                <ToggleButton value={"check"} selected={darkMode} onChange={toggleDarkMode}>
-                    {darkMode ? <LightMode/> : <DarkMode />}
-                </ToggleButton>
-            </div>
-        </div>
-    )
-}
+  const [open, setOpen] = useState<boolean>(false);
+  const { darkMode, toggleDarkMode } = useContext(ThemeContext);
 
-export default Navbar
+  const ToggleOpenMenu = () => {
+    setOpen((prevState) => !prevState);
+  };
+
+  const menuStyles = open ? "menu open" : "menu";
+
+  return (
+    <div className="navbar">
+      <div className="brand">
+        <span>Resume Management</span>
+      </div>
+      <div className={menuStyles}>
+        <ul>
+          {links.map((item) => (
+            <li key={item.href} onClick={ToggleOpenMenu}>
+              <Link to={item.href}>{item.label}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="hamburger">
+        <Menu onClick={ToggleOpenMenu} />
+      </div>
+      <div className="toggle">
+        <ToggleButton
+          value={"check"}
+          selected={darkMode}
+          onChange={toggleDarkMode}>
+          {darkMode ? <DarkMode /> : <LightMode />}
+        </ToggleButton>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
