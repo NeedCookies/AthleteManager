2cd7311d9ba82efbc90aa5de04312330fdd7a1dd
NeedCookies <emilkalim895@gmail.com>
2024-04-25 22:52:53 +0300

    Finish base home page (Add progress line and page template)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,27 @@
-import { useContext } from 'react';
-import { ThemeContext } from "./context/theme.context"
-import Navbar from './components/navbar/Navbar.component';
+import { useContext, lazy, Suspense } from "react";
+import { ThemeContext } from "./context/theme.context";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar.component";
+import CustomLinearProgress from "./components/CustomLinearProgress.component";
+
+//Lazy loading
+const Home = lazy(() => import("./pages/Home/Home.page"));
 
 const App = () => {
-  const {darkMode} = useContext(ThemeContext);
+  const { darkMode } = useContext(ThemeContext);
 
-  const appStyles = darkMode ? "app dark" : "app"
-  
+  const appStyles = darkMode ? "app dark" : "app";
+
   return (
     <div className={appStyles}>
       <Navbar />
-      <div className='wrapper'>Routes</div>
+      <div className="wrapper">
+        <Suspense fallback={<CustomLinearProgress />}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Suspense>
+      </div>
     </div>
   );
 };
---
+++ frontend/src/pages/Home/Home.page.tsx
@@ -0,0 +1,26 @@
+import "./home.scss";
+
+const Home = () => {
+  return (
+    <div className="content home">
+      <h3>Country sports archive website</h3>
+      <br />
+      <br />
+      <span>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto placeat
+        nisi illo modi asperiores explicabo velit, dolores maxime perspiciatis
+        obcaecati, dolorum odit ab cum harum quaerat et dolore. Nobis laborum
+        enim labore consequuntur magni, cupiditate dignissimos praesentium saepe
+        libero corrupti possimus, quos consectetur exercitationem perspiciatis?
+        Minima, atque autem iure commodi voluptatum ratione tempora ab quisquam
+        vitae maiores nihil aut numquam laboriosam facilis qui totam eum. Vitae
+        dignissimos aliquam praesentium dolorem voluptatem reprehenderit
+        tempore? Dolores ab itaque quidem et non maxime quia voluptas dolor
+        saepe nesciunt consequuntur harum, pariatur, maiores atque. Nulla itaque
+        rerum reprehenderit accusamus qui atque eaque, consequuntur quisquam.
+      </span>
+    </div>
+  );
+};
+
+export default Home;
